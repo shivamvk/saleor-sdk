e517fae05f0a0b6bd42ae7f936abfa95d556b202
cristian05 <cristianflorez0501@gmail.com>
2021-04-28 00:53:48 -0500

    refactor(const): disable browser relate code
    * `window` though is not empty. It's set to global in react native
    so its better to check document object. This avoids that we have to
    remove references to WINDOW_EXIST in saleor-skd code, this change
    simply makes the code that uses this variable not be executed.
    
    * Any reference to `LOCAL_STORAGE_EXISTS` variable was deleted.
    
    * Therefore `CREDENTIAL_API_EXISTS` variables will be initialized with false.

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,5 @@
-export const WINDOW_EXISTS = typeof window !== "undefined";
+// It's react-native environment
+export const WINDOW_EXISTS = typeof document !== "undefined";
 
 export const LOCAL_STORAGE_EXISTS = WINDOW_EXISTS && !!window.localStorage;
 export const CREDENTIAL_API_EXISTS =
