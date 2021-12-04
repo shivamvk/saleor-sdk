4f96aae1f317e752d23f1ab19a0b57be43584b27
shivamvk <shivam.vk529@gmail.com>
2021-12-04 17:30:52 +0530

    add metadata to cart items

--- src/fragments/checkout.ts
+++ src/fragments/checkout.ts
@@ -46,6 +46,10 @@ export const checkoutProductVariantFragment = gql`
     images {
       url
     }
+    metadata {
+      key
+      value
+    }
     pricing {
       onSale
       priceUndiscounted {
